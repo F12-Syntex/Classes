7dbc97a63e714bf2ffc1597dbba6781bf11eb8eb
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2022-02-13 15:11:55 +0300

    L1
    The end of lesson 1

---
+++ ShouibClass/src/com/syntex/lesson1/Homework.java
@@ -0,0 +1,36 @@
+package com.syntex.lesson1;
+
+public class Homework {
+
+	public static void main(String[] args) {
+		
+		//example
+		System.out.println("6 x 1 = " + 6*1);
+		System.out.println("6 x 2 = " + 6*2);
+		System.out.println("6 x 3 = " + 6*3);
+		
+		
+		
+		Homework.homework1();
+		Homework.homework2();
+		
+	}
+	
+	
+	/*
+	 * Print the 5 times table. up to 5x12. 
+	 * System.out.println("5 x 1 = " + 5*1);
+	 */
+	public static void homework1() {
+		
+	}
+	
+	/*
+	 * Make 5 variables, and them add them all.
+	 * print the sum
+	 */
+	public static void homework2() {
+		
+	}
+	
+}
--- ShouibClass/src/com/syntex/lesson1/Program.java
+++ ShouibClass/src/com/syntex/lesson1/Program.java
@@ -2,6 +2,134 @@ package com.syntex.lesson1;
 
 public class Program {
 
+
+	public static void main(String[] args) {
+
+		//<type> <name> = <data>
+		
+		//4 Primitive data types
+		
+		//String >> Text
+		//Double >> Decimals
+		//Int >> Whole numbers
+		//Boolean >> Yes or No ( True or False ) 
+		
+		
+		//  (1)    (2)  =  (3)
+		//<type> <name> = <data>;
+		
+		double decimals = 53.1;
+		double examplee = 3.14;
+		
+		String valorant = "i like valorant!";
+		String apex = "i like apex"; 
+		
+		int four = 4; 
+		int six = 6;	
 	
+		boolean leevle = true;
+	
+		double maths = 5 + 6;
+	
+		
+		//System.out.println(maths);
+		double testy = 5 - 1000;
+		//System.out.println(testy);
+		
+		double sum = maths + testy;
+		
+		//System.out.println(sum);
+		
+		//Program.practice1();
+		//Program.practice2();
+		//Program.practice3();
+		Program.practice4();
+		
+	}
+	
+	/*
+	 * Make a variable called a and b.
+	 * Make a = to a random positive number
+	 * Make b = to a random negative number
+	 * Then make a new variable called sum, and initialise it to the sum of a and b
+	 * Then print the variable
+	 */
+	public static void practice1() {
+		
+	}
+	
+	/*
+	 * Make a variable and set it to a random string, then print it 5 times then 5 times again with a new line in between
+	 */
+	public static void practice2() {
+		
+	}
+	
+	/*
+	 * Make a calculator using 3 variables; a, b, c, c needs to equal ( a - b ) + ( a + b );
+	 * Then print it
+	 */
+	public static void practice3() {
+		
+		String ay = "a";
+		String be = "b";
+		String ce = "c";
+		double a =  141;
+		double b = 421;
+		double c = ( a - b ) + ( a + b );
+		System.out.println(c);
+		
+		
+		
+		
+		
+	}
+	
+	/*
+	 * Print the 5 times table. up to 5x12. 
+	 * System.out.println("5 x 1 = " + 5*1);
+	 */
+	public static void practice4() {
+		
+	//question 1	
+	String f = "a";	
+	double a = 10 + 6;
+	String g = "b";
+	double b = 6-20;
+	double sum = a + b;
+	
+	
+	System.out.println(sum);
+	//question 2
+	String random1 = "valorant";
+	String random2 = "valorant";
+	String random3 = "valorant";
+	String random4 = "valorant";
+	String random5 = "valorant";	
+	
+	String random6 = "valorant";
+	String random7 = "valorant";
+	String random8= "valorant";
+	String random9 = "valorant";
+	String random10 = "valorant";
+	System.out.println(random1);
+	System.out.println(random2);
+	System.out.println(random3);
+	System.out.println(random4);
+	System.out.println(random5);
+	System.out.println("");
+	System.out.println(random6);
+	System.out.println(random7);
+	System.out.println(random8);
+	System.out.println(random9);
+	System.out.println(random10);
+			
+
+	}
+
+
 	
 }
+
+	
+	
