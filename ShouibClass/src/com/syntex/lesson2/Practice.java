30e938500d6e4d534e779cf3e3bf32c405c2ac5b
Syntex <30322632+F12-Syntex@users.noreply.github.com>
2022-02-17 13:59:29 +0300

    Lesson 2

---
+++ ShouibClass/src/com/syntex/lesson2/Practice.java
@@ -0,0 +1,26 @@
+package com.syntex.lesson2;
+
+public class Practice {
+
+	//make the main method
+	//make a variable called test, which is a string "HELLO"
+	//then print that variable
+	
+	public static void main(String[] args) {
+	
+		String HELLO = "test";
+		System.out.println(HELLO);
+		
+		
+		
+		double t = 5;
+		double g = 6;
+		double tg = t + g;
+		System.out.println(tg);
+		
+		
+	}
+			
+			
+			
+	}
---
+++ ShouibClass/src/com/syntex/lesson2/Program.java
@@ -0,0 +1,63 @@
+package com.syntex.lesson2;
+
+public class Program {
+
+
+	public static void main(String[] args) {
+
+		//<type> <name> = <data>
+		
+		//4 Primitive data types
+		
+		//String >> Text
+		//Double >> Decimals
+		//Int >> Whole numbers
+		//Boolean >> Yes or No ( True or False ) 
+		
+		
+		//  (1)    (2)  =  (3)
+		//<type> <name> = <data>;
+		
+		
+		for(int a = 0; a < 10; a++) {
+			System.out.println(a);
+		}
+		
+		// print hello world 25 times.
+		for(int helloworld = 0; helloworld < 10; helloworld++) {
+			System.out.println("helloworld" +  helloworld);	
+		}
+		
+		for(int ilikechickennuggets = 0; ilikechickennuggets < 30; ilikechickennuggets++) {
+		
+			System.out.println("ilikechickennuggets x" + ilikechickennuggets);
+		}
+		
+		for (int helloworld = 0; helloworld < 50; helloworld++) {
+			System.out.println("helloworld x" + helloworld);
+			
+		}
+		for(int valorant = 0; valorant < 50; valorant++) {
+		System.out.println("valorant x" + valorant);	
+		}
+		
+		for(int a = 0; a < 50; a++) {
+		
+			int b = 15 * a;
+		
+			System.out.println(b);
+			
+		}
+		
+	 for(int b = 0; b < 10; b++) {
+	int c = 10 * b;	
+	System.out.println(c);	
+	
+			}	
+	 
+	}
+	 }
+			
+
+	
+	
